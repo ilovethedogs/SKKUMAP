c7880c6bea3f901aa8a8d9d869c8eb2ac3f2b890
Jaein Park <jaynap@g.skku.edu>
2022-11-15 17:33:38 +0900

    레이아웃 바꾸기 스크립트 조금 쓰기

--- app/src/main/java/swe2022/team6/skkumap/dataclasses/UserSetting.java
+++ app/src/main/java/swe2022/team6/skkumap/dataclasses/UserSetting.java
@@ -1,8 +1,8 @@
 package swe2022.team6.skkumap.dataclasses;
 
 public class UserSetting {
-    private boolean field1;
-    private boolean field2;
-    private boolean field3;
-    private int     field4;
+    private boolean notiActivate=false;
+    private int     notiTime=0; //int로 분 표현
+    private boolean notiLoc=false;
+    private int notiMthd=0;//0: vibration 1: sound 2: push
 }
--- app/src/main/java/swe2022/team6/skkumap/fragments/SettingFragments.java
+++ app/src/main/java/swe2022/team6/skkumap/fragments/SettingFragments.java
@@ -1,22 +1,38 @@
 package swe2022.team6.skkumap.fragments;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 import swe2022.team6.skkumap.R;
+import swe2022.team6.skkumap.databinding.FragmentSettingFragmentsBinding;
+import swe2022.team6.skkumap.databinding.FragmentTimetableFragmentsBinding;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link SettingFragments#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class SettingFragments extends Fragment {
 
+
+public class SettingFragments extends Fragment {
+    FragmentSettingFragmentsBinding binding;
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -48,19 +64,48 @@ public class SettingFragments extends Fragment {
         return fragment;
     }
 
+    AlertDialog alertDialog;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+        LayoutInflater inflater = requireActivity().getLayoutInflater();
+        View v = inflater.inflate(R.layout.fragment_setting_fragments, null);
+
+
+
+
+
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_setting_fragments, container, false);
+        binding= FragmentSettingFragmentsBinding.inflate(inflater, container, false);
+        binding.tpNoti.setIs24HourView(true);
+        binding.swtchNotiActivate.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                                                      @Override
+                                                      public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                                                          Log.e("TAG", "switch checked");
+                                                          Toast.makeText(getActivity().getApplicationContext(), "switch", Toast.LENGTH_SHORT).show();
+                                                          if(b){
+
+                                                          }
+                                                          else{
+
+                                                          }
+                                                      }
+                                                  }
+
+        );
+//        return inflater.inflate(R.layout.fragment_setting_fragments, container, false);
+        return binding.getRoot();
     }
 }
